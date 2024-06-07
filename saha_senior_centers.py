bc1356ca0ff8f16cace0670ed68ec248fa8a662e
gandareman <memo7748@gmail.com>
2024-06-08 08:28:14 +0900

    a

--- saha_senior_centers.py
+++ saha_senior_centers.py
@@ -1,10 +1,12 @@
 import os
+import base64
 import pandas as pd
 import geopandas as gpd
 import folium
 from folium.plugins import MarkerCluster
 import streamlit as st
 from streamlit_folium import folium_static
+from io import BytesIO
 
 # 스트림릿 페이지 설정
 st.set_page_config(page_title="사하구 경로당 현황", layout="wide")
@@ -18,7 +20,7 @@ def load_data(uploaded_file):
     df = pd.read_excel(uploaded_file)
     if '일자' in df.columns:
         df = df[df['일자'].notnull()]  # 일자 열의 NaN 값 제거
-        df['일자'] = pd.to_datetime(df['일자'], errors='coerce').dt.year  # 일자 열을 datetime으로 변환 후 연도로 변환
+        df['일자'] = pd.to_datetime(df['일자'], errors='coerce')  # 일자 열을 datetime으로 변환
     return df
 
 # 지오JSON 로드 함수
@@ -39,9 +41,13 @@ with col_1:
     # 엑셀 파일 업로드 위젯
     uploaded_file = st.file_uploader("엑셀 파일 업로드", type=["xlsx"])
 
+# 세션 상태에 파일 저장
 if uploaded_file is not None:
-    # 데이터 로드
-    df = load_data(uploaded_file)
+    st.session_state['uploaded_file'] = uploaded_file
+
+# 세션 상태에 파일이 저장되어 있으면 로드
+if 'uploaded_file' in st.session_state:
+    df = load_data(st.session_state['uploaded_file'])
 
     # 마커 클러스터링 여부 선택 옵션
     use_marker_cluster = st.sidebar.radio("마커 클러스터 사용 여부", ("사용", "사용 안 함"))
@@ -60,27 +66,40 @@ if uploaded_file is not None:
     # 동적 필터링 옵션
     selected_filters = {}
     for filter_col in st.session_state['filters']:
-        unique_values = df[filter_col].dropna().unique()
-        selected_values = st.sidebar.multiselect(f"{filter_col}", unique_values, default=unique_values)
-        selected_filters[filter_col] = selected_values
+        if filter_col == '전체':
+            selected_filters.clear()
+            break
+        unique_values = df[filter_col].dropna().unique().tolist()
+        unique_values.insert(0, '전체')
+        # selected_values = st.sidebar.multiselect(f"{filter_col}", unique_values, default=unique_values)
+        selected_values = st.sidebar.multiselect(f"{filter_col}", unique_values, default=['전체'])
+        if '전체' in selected_values:
+            selected_filters[filter_col] = unique_values[1:]  # '전체'를 제외한 모든 값을 선택
+        else:
+            selected_filters[filter_col] = selected_values
 
     # 건축 연도 옵션
-    if '일자' in df.columns:
-        min_year = int(df['일자'].min())
-        max_year = int(df['일자'].max())
+    if '일자' in df.columns and '전체' not in st.session_state['filters']:
+        min_year = df['일자'].dt.year.min()
+        max_year = df['일자'].dt.year.max()
         selected_year_range = st.sidebar.slider("건축 연도", min_year, max_year, (min_year, max_year))
-        filtered_df = df[(df['일자'] >= selected_year_range[0]) & (df['일자'] <= selected_year_range[1])]
+        filtered_df = df[(df['일자'].dt.year >= selected_year_range[0]) & (df['일자'].dt.year <= selected_year_range[1])]
     else:
         filtered_df = df.copy()
 
     # 필터링
-    for filter_col, selected_values in selected_filters.items():
-        filtered_df = filtered_df[filtered_df[filter_col].isin(selected_values)]
+    if '전체' not in st.session_state['filters']:
+        for filter_col, selected_values in selected_filters.items():
+            filtered_df = filtered_df[filtered_df[filter_col].isin(selected_values)]
+
 
     st.sidebar.markdown("---")
     st.sidebar.header("팝업 옵션")
+
     # 팝업에 표시할 필드 선택
-    popup_fields = st.sidebar.multiselect("팝업에 표시할 필드 선택", df.columns.tolist(), default=["시설명", "주소"])
+    available_columns = df.columns.tolist()
+    default_popup_fields = [col for col in ["시설명", "주소"] if col in available_columns]
+    popup_fields = st.sidebar.multiselect("팝업에 표시할 필드 선택", available_columns, default=default_popup_fields)
 
     if not filtered_df.empty:
         # 경로당 위치의 위도와 경도의 평균값 계산
@@ -105,9 +124,12 @@ if uploaded_file is not None:
         for idx, row in filtered_df.iterrows():
             if not pd.isna(row['위도']) and not pd.isna(row['경도']):
                 # 팝업 HTML 설정
-                popup_html = f"<div style='width: 200px; height: auto;'>"
+                popup_html = f"<div style='width: 250px; height: auto;'>"
                 for field in popup_fields:
-                    popup_html += f"<h4>{field}: {row[field]}</h4>"
+                    value = row[field]
+                    if isinstance(value, pd.Timestamp):
+                        value = value.strftime('%Y-%m-%d')
+                    popup_html += f"<p style='color: blue; font-size: 12px;'>{field}: {value}</p>"
                 popup_html += "</div>"
 
                 # 툴팁을 항상 보이도록 설정
@@ -116,7 +138,7 @@ if uploaded_file is not None:
                 marker = folium.Marker(
                     location=[row['위도'], row['경도']],
                     tooltip=tooltip,
-                    popup=folium.Popup(popup_html, max_width=250)
+                    popup=folium.Popup(popup_html, max_width=400)
                 )
 
                 if use_marker_cluster == "사용":
@@ -131,8 +153,15 @@ if uploaded_file is not None:
         # 지도 표시
         folium_static(m, width=1200, height=800)
 
+        # 지도 다운로드 링크 생성
+        map_html = m.get_root().render()
+        map_html_bytes = map_html.encode('utf-8')
+        b64 = base64.b64encode(map_html_bytes).decode('utf-8')
+        href = f'<a href="data:text/html;base64,{b64}" download="saha_senior_centers_map.html">지도 다운로드</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
         # 데이터프레임 전체 출력
-        # st.dataframe(filtered_df, use_container_width=True, width=800)
+        filtered_df['일자'] = filtered_df['일자'].dt.strftime('%Y-%m-%d')
         st.dataframe(filtered_df, width=1200)
     else:
         st.warning("선택된 필터에 맞는 데이터가 없습니다. 다른 필터를 선택해 주세요.")
